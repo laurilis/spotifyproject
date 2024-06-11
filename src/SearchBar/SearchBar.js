a44c405ae2e36d293819fc4cc72d3fb20bc8965e
Laurili <laura.pezzullo@posteo.de>
2024-06-11 12:03:52 +0200

    commit changes

--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './SearchBar.module.css';
 
+
 function SearchBar() {
+
+    const [ input, setInput ] = useState('');
+
+
+
+    const handleInputChange = (e) => {
+        setInput(e.target.value)
+    };
+
     return (
-        <>
-        <input className='SearchInput' />
-        <button className='SearchButton'>Search</button>
-        </>
+        <div className='SearchBar'>
+              <input 
+                
+                type="text" 
+                value={input}
+                onChange={handleInputChange} 
+            />
+            <button 
+                className="SearchButton" 
+            >Search</button>
+        </div>
     )
 
 };
